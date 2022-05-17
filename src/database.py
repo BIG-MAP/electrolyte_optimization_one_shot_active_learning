d5de56ee3d75f11c9d6c95e930f57b70e0603109
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-05-17 14:39:59 +0200

    small modification in data config

--- src/database.py
+++ src/database.py
@@ -5,19 +5,23 @@ class Database:
             and get their corresponding settings
         """
         # dataset before one shot active learning 
+        self._raw_initial_data = load_dataset(dataset_type="original", name="OriginalDataRaw.csv", seperator=True)
         self._initial_data = load_dataset(dataset_type="original", name="OriginalData.csv")
         # dataset after one shot active learning 
         self._updated_data = load_dataset(dataset_type="original", name="OriginalData2.csv")
         # the added electrolyte formulations
-        self._added_formulation = load_dataset(dataset_type="formulation_suggestions", name="comparison_experiment_finale.csv")
+        self._raw_added_formulations = load_dataset(dataset_type="original", name="ElectrolytepredictionAfterOneShotRaw.csv", seperator=True)
+        self._added_formulations = load_dataset(dataset_type="formulation_suggestions", name="comparison_experiment_finale.csv")
         # extracting the temperatures and sort them 
         self.temperatures = self._updated_data["temperature"].unique().tolist()
         self.temperatures.sort()
         self.sorted_temperatures = self.temperatures
         # specify the input & output columns used for training
-        self.input_column = ["PcEcPc", "liEcPc"]
+        self.input_columns = ["PcEcPc", "liEcPc"]
         self.output_column = ["conductivity"]
     
     def __str__(self):
         return f"Variaty of electrolyte formulations for LIBs"
 
+# c = Database()
+# print(c)
